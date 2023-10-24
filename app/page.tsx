0fbcfe1638f2653a4c4e21f6535686c107e9d615
Noa Gosselin <noa.gosselin@ribpay.app>
2023-10-24 15:17:29 +0200

    Fix:Apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import Image from 'next/image'
-import { TypeAnimation } from 'react-type-animation';
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const setStyle = () => {
-    return({fontSize: '2em', display: 'inline-block', color:'black'})
-}
+  return { fontSize: "2em", display: "inline-block", color: "black" };
+};
 
 const ExampleComponent = () => {
   return (
@@ -17,22 +17,25 @@ const ExampleComponent = () => {
         "Gosselin",
         1000,
         "GOSSELIN",
-        1000
+        1000,
       ]}
       wrapper="span"
       speed={50}
       repeat={1}
     />
-    )}
+  );
+};
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center">
-        <h2 className="text-6xl text-center" style={setStyle()}  >Je m'appelle Noa <ExampleComponent/></h2>
+        <h2 className="text-6xl text-center" style={setStyle()}>
+          Je m appelle Noa <ExampleComponent />
+        </h2>
         <h3 className="text-2xl text-center">h3</h3>
         <h4 className="text-2xl text-center">h4</h4>
       </div>
     </main>
-  )
+  );
 }
