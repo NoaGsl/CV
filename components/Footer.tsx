62b3321935577b285fde59883121d09cb257c5b4
Noa Gosselin <noa.gosselin@ribpay.app>
2023-11-27 12:11:07 +0100

    added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+    <div className="absolute w-full inset-x bottom-0 h-1/10 bg-light_brown text-center">
+        <p className="text-2xl text-black mt-0">Noa Gosselin</p>
+    </div>
+    );
+}
